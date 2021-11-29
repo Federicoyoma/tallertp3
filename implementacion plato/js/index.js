c2f0aec25b758c4399d86e50ec9bae18d5477434
Federicoyoma <66265241+Federicoyoma@users.noreply.github.com>
2021-11-29 13:10:59 -0300

    Add files via upload

---
+++ implementacion plato/js/index.js
@@ -0,0 +1,24 @@
+function OpcionesParaFrutas() {
+  document.getElementById('OpcionesFrutas').style.display = 'block';
+}
+
+function OcultarOpcionesParaFrutas() {
+  document.getElementById('OpcionesFrutas').style.display = 'none';
+}
+
+
+function Naranja() {
+  document.getElementById('PlatoConFrutas').style.display = 'block';
+}
+
+function Limon() {
+  document.getElementById('PlatoConFrutasYLimon').style.display = 'block';
+}
+
+function Frutilla() {
+  document.getElementById('PlatoConFrutasYFrutilla').style.display = 'block';
+}
+
+function Manzana() {
+  document.getElementById('PlatoConFrutasYManzana').style.display = 'block';
+}
