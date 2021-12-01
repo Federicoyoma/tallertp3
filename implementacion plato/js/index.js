79192f06e11a0dd648fe26559ae14ccc8ba34e01
Federicoyoma <66265241+Federicoyoma@users.noreply.github.com>
2021-12-01 16:54:46 -0300

    Add files via upload

--- implementacion plato/js/index.js
+++ implementacion plato/js/index.js
@@ -6,19 +6,31 @@ function OcultarOpcionesParaFrutas() {
   document.getElementById('OpcionesFrutas').style.display = 'none';
 }
 
-
 function Naranja() {
   document.getElementById('PlatoConFrutas').style.display = 'block';
+  document.getElementById('PlatoConFrutasYLimon').style.display = 'none';
+  document.getElementById('PlatoConFrutasYFrutilla').style.display = 'none';
+  document.getElementById('PlatoConFrutasYManzana').style.display = 'none';
 }
 
 function Limon() {
   document.getElementById('PlatoConFrutasYLimon').style.display = 'block';
+  document.getElementById('PlatoConFrutas').style.display = 'none';
+  document.getElementById('PlatoConFrutasYFrutilla').style.display = 'none';
+  document.getElementById('PlatoConFrutasYManzana').style.display = 'none';
+
 }
 
 function Frutilla() {
   document.getElementById('PlatoConFrutasYFrutilla').style.display = 'block';
+  document.getElementById('PlatoConFrutas').style.display = 'none';
+  document.getElementById('PlatoConFrutasYLimon').style.display = 'none';
+  document.getElementById('PlatoConFrutasYManzana').style.display = 'none';
 }
 
 function Manzana() {
   document.getElementById('PlatoConFrutasYManzana').style.display = 'block';
+  document.getElementById('PlatoConFrutasYFrutilla').style.display = 'none';
+  document.getElementById('PlatoConFrutas').style.display = 'none';
+  document.getElementById('PlatoConFrutasYLimon').style.display = 'none';
 }
